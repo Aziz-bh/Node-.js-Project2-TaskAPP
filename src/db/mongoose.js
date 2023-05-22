fd854c614efb87b61713d6e8b160d63bc8ff719b
Aziz benhmida <benhmida.mohamedaziz@esprit.tn>
2023-05-22 14:12:51 +0100

    add validator to models

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
@@ -7,24 +8,71 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
 const User = mongoose.model("User", {
   name: {
     type: String,
+    required: true,
+    trim: true,
+    unique: true,
   },
   age: {
     type: Number,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be positive");
+      }
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email must be a valid email address");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("error passowrd conatains the password word");
+      }
+    },
   },
 });
+
+const me = new User({
+  name: "aziz",
+  age: 24,
+  email: "email@email.com",
+  password: "paass123spttt",
+});
+
+// me.save()
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
 
 const Task = mongoose.model("Task", {
   description: {
     type: String,
+    required: true,
+    trim: true,
   },
   completed: {
     type: Boolean,
+    default: false,
   },
 });
 
 const task1 = new Task({
-  description: "task 1 description 1",
-  completed: true,
+
 });
 
 const task2 = new Task({
@@ -45,30 +93,18 @@ task1
   .catch((error) => {
     console.log(error);
   });
-task2
-  .save()
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-task3
-  .save()
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-const me = new User({
-  name: "aziz",
-  age: 24, // Assign a number value instead of a string
-});
-
-// me.save()
-//   .then((me) => {
-//     console.log(me);
+// task2
+//   .save()
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+// task3
+//   .save()
+//   .then((result) => {
+//     console.log(result);
 //   })
 //   .catch((error) => {
 //     console.log(error);
