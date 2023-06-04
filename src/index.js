565eda284cb30cfaf885cf64ed03a066c836da01
Aziz benhmida <benhmida.mohamedaziz@esprit.tn>
2023-06-04 21:38:46 +0100

    Password hashing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 const express = require("express");
 require("./db/mongoose");
 const bodyParser = require("body-parser");
-
-const Task = require("./models/task");
 const routerUser = require("./routers/user");
 const routerTask = require("./routers/task");
-const User = require("./models/user");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -16,11 +13,12 @@ app.listen(port, () => {
   console.log("server is up and running at port" + port);
 });
 
-const updateAgeAndCount = async (id, age) => {
-  const user = await User.findByIdAndUpdate(id, { age });
-  const count = await User.countDocuments({ age });
-  return count;
-};
+
+// const updateAgeAndCount = async (id, age) => {
+//   const user = await User.findByIdAndUpdate(id, { age });
+//   const count = await User.countDocuments({ age });
+//   return count;
+// };
 
 // updateAgeAndCount("646b64dd78697f62719e702d", 2)
 //   .then((count) => {
@@ -30,16 +28,16 @@ const updateAgeAndCount = async (id, age) => {
 //     console.log(e);
 //   });
 
-const deleteAndCountTask = async (id) => {
-  await Task.findByIdAndDelete(id);
-  const count = await Task.countDocuments();
-  return count;
-};
+// const deleteAndCountTask = async (id) => {
+//   await Task.findByIdAndDelete(id);
+//   const count = await Task.countDocuments();
+//   return count;
+// };
 
-deleteAndCountTask("646b7fc8e46d7a4d152964fe")
-  .then((r) => {
-    console.log(r);
-  })
-  .catch((e) => {
-    console.log(e);
-  });
+// deleteAndCountTask("646b7fc8e46d7a4d152964fe")
+//   .then((r) => {
+//     console.log(r);
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
--- src/routers/task.js
+++ src/routers/task.js
@@ -68,7 +68,13 @@ router.patch("/tasks/:id", async (req, res) => {
     return res.status(400).send({ error: "Invalid updates !" });
   }
   try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body);
+    const task = await Task.findById(req.params.id);
+
+    updates.forEach((update) => {
+      task[update] = req.body[update];
+    });
+    await task.save();
+
     if (!task) {
       res.status(404).send();
     }
